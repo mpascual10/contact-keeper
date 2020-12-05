8cfc5c0b895c78e5db0ca5a4369e32d2f67d0744
mpascual10 <1marc.pascual@gmail.com>
2020-12-05 09:33:25 -0500

    prepare for deloy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); //brings in out express server
 const connectDB = require('./config/db');
 const app = express(); //initializes express in a variable
+const path = require('path');
 
 //this is a test response for any incoming requests to localhost:5000
 //app.get('/', (req,res) => res.send('farting is nothing to be embarassed about'));
@@ -25,6 +26,18 @@ app.get('/', (req,res) => res.json({
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//serve static assets in production
+
+if(process.env.NODE_ENV === 'production')
+{
+    //set static build folder
+    app.use(express.static('client/build'));
+
+    //app.get('*') is get anything that isnt above
+    //resolve method looks in '__dirname' current directory, then 'client''build' build folder then loads index
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000; //looks for environment variable called port first that used in production //dev uses 5000
 
